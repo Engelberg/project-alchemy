620375ad432f2b101e07e47b5ac6dabd49ae0904
Mark Engelberg <mark.engelberg@gmail.com>
2019-03-27 07:08:24 -0700

    Removed separate record for secp256k1 points

--- src/project_alchemy/ecc.clj
+++ src/project_alchemy/ecc.clj
@@ -3,8 +3,6 @@
   (:require [clojure.math.numeric-tower :as nt]
             [better-cond.core :refer [cond defnc defnc-]]))
 
-(def A 0)
-(def B 7)
 (def P (clojure.core/- (nt/expt 2 256) (nt/expt 2 32) 977))
 (def N 0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141)
 (declare S256Point?)
@@ -61,6 +59,11 @@
   (assert (and (<= 0 num) (< num prime)))
   (FieldElement. (biginteger num) (biginteger prime)))
 
+(defn pprint-FieldElement [{:keys [num prime] :as f}]
+  (if (not= prime P) (pr f)
+      (println (format "0x%064x" num))))
+(. clojure.pprint/simple-dispatch addMethod FieldElement pprint-FieldElement)
+
 (defrecord Point [x y a b]
   FieldOps
   (zero [p] (Point. nil nil a b))
@@ -88,41 +91,16 @@
   (assert (valid-point? x y a b) "Point not on curve")
   (Point. x y a b))
 
-(defrecord S256Point [x y]
-  FieldOps
-  (zero [p] (S256Point. nil nil))
-  (+ [p1 {x2 :x y2 :y :as p2}]
-    (cond
-      (nil? x) p2
-      (nil? x2) p1
-      (and (= x x2) (not= y y2)) (S256Point. nil nil)
-      (= p1 p2) (if (= y (zero x)) (S256Point. nil nil)
-                    (let [s (/ (* 3 (expt x 2)) (* 2 y)),
-                          x3 (- (expt s 2) (* 2 x))
-                          y3 (- (* s (- x x3)) y)]
-                      (S256Point. x3 y3)))
-      :else (let [s (/ (- y2 y) (- x2 x)),
-                  x3 (- (- (expt s 2) x) x2),
-                  y3 (- (* s (- x x3)) y)]
-              (S256Point. x3 y3)))))
+(def A (->FieldElement 0 P))
+(def B (->FieldElement 7 P))
 
 (defn ->S256Point [x y]
-  (let [x (->FieldElement x P), y (->FieldElement y P) A (->FieldElement A P), B (->FieldElement B P)]
+  (let [x (when x (->FieldElement x P)), y (when y (->FieldElement y P))]
     (assert (valid-point? x y A B) "Point not on curve")
-    (S256Point. x y)))
+    (Point. x y A B)))
 
-(defn S256Point? [p] (instance? S256Point p))
+(defn S256Point? [{:keys [a b]}] (and (= a A) (= b B)))
 
 (def G (->S256Point
         0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
         0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8))
-
-(defn pprint-256point [p]
-  (let [^BigInteger x (:num (:x p)),
-        ^BigInteger y (:num (:y p))]
-    (if (nil? x) (println "S256Point(infinity)")
-        (do (println "S256Point(")
-            (println (str " 0x" (.toString x 16) ","))
-            (println (str " 0x" (.toString y 16) ")"))))))
-
-(. clojure.pprint/simple-dispatch addMethod S256Point pprint-256point)
