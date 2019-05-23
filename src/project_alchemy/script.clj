c791fb495341ddf703e2f23a05cb5d283a486ded
Mark Engelberg <mark.engelberg@gmail.com>
2019-05-23 00:53:31 -0700

    Segwit - chap 13

--- src/project_alchemy/script.clj
+++ src/project_alchemy/script.clj
@@ -4,10 +4,13 @@
             [project-alchemy.helper :refer [read-bytes read-varint encode-varint le-bytes->num le-num->bytes hash256 unsigned-byte]]
             [buddy.core.bytes :as bytes]
             [buddy.core.codecs :refer :all]
+            [buddy.core.hash :as h]
             [project-alchemy.op :as op :refer [op-code-functions op-code-names]]
             [clojure.tools.logging :as log]
             [clojure.java.io :as io]
-            [project-alchemy.ecc :as ecc])
+            [project-alchemy.ecc :as ecc]
+            [project-alchemy.helper :as helper]
+            [project-alchemy.script :as script])
   (:import java.io.InputStream java.util.Stack))
 
 (defnc parse-script [^InputStream stream]
@@ -61,7 +64,52 @@
 ;; Book notes that the following evaluation method is not totally safe because
 ;; it fails to implement proper isolation between sig and pubkey
 
-(defnc evaluate-script [script z]
+(defnc p2sh-rule [[cmd0 cmd1 cmd2 :as script] ^Stack stack cmd]
+  (and (= (count script) 3)
+       (= cmd0 0xa9)
+       (bytes? cmd1)
+       (= (count cmd1) 20)
+       (= cmd2 0x87))
+  (cond
+    (not (op/op-hash160 stack)) false
+    :do (.push stack cmd1)
+    (not (op/op-equal stack)) false
+    (not (op/op-verify stack))  (do (log/info "bad p2sh h160") false)
+    :let [redeem-script (bytes/concat (encode-varint (count cmd))
+                                      cmd)]
+    (parse-script (io/input-stream redeem-script)))
+  script)
+
+(declare p2pkh-script)
+(defnc p2wpkh-rule [script ^Stack stack witness]
+  :let [vstack (vec stack)]
+  (and (= (count stack) 2) (bytes/equals? (nth vstack 0) (byte-array []))
+       (= (count (nth vstack 1)) 20))
+  (let [h160 (.pop stack),
+        _ (.pop stack)]
+    (concat script witness (p2pkh-script h160)))
+  script)
+
+(defnc p2wsh-rule [script ^Stack stack witness]
+  :let [vstack (vec stack)]
+  (and (= (count stack) 2) (bytes/equals? (nth vstack 0) (byte-array []))
+       (= (count (nth vstack 1)) 32))
+  (cond
+    :let [s256 (.pop stack),
+          _ (.pop stack)
+          script (concat script (drop-last 1 witness))
+          witness-script (last witness)]
+    (not (bytes/equals? s256 (h/sha256 witness-script)))
+    (do (log/info "bad sha256" (bytes->hex s256)
+                  (bytes->hex (h/sha256 witness-script)))
+        false)
+    (concat script (script/parse-script (io/input-stream
+                                         (bytes/concat
+                                          (encode-varint (count witness-script))
+                                          witness-script)))))
+  script)
+
+(defnc evaluate-script [script z witness]
   :let [stack (Stack.) altstack (Stack.),
         result (loop [script (seq script)]
                  (cond
@@ -70,20 +118,10 @@
                    (not (number? cmd))
                    (cond
                      :do (.push stack cmd)
-                     :let [[cmd0 cmd1 cmd2] script]
-                     (not (and (= (count script) 3)
-                               (= cmd0 0xa9)
-                               (bytes? cmd1)
-                               (= (count cmd1) 20)
-                               (= cmd2 0x87)))
-                     (recur script),
-                     (not (op/op-hash160 stack)) false
-                     :do (.push stack cmd1)
-                     (not (op/op-equal stack)) false
-                     (not (op/op-verify stack)) (do (log/info "bad p2sh h160") false)
-                     :let [redeem-script (bytes/concat (encode-varint (count cmd))
-                                                       cmd)]
-                     (recur (parse-script (io/input-stream redeem-script))))
+                     :let [script (p2sh-rule script stack cmd),
+                           script (p2wpkh-rule script stack witness),
+                           script (p2wsh-rule script stack witness)]
+                     (recur script))
                    :let [op (op-code-functions cmd)]
                    (#{99 100} cmd) (let [a-items (atom script)]
                                      (if-not (op stack a-items)
@@ -105,7 +143,7 @@
   (= (count stack) 0) false
   (= (count (.pop stack)) 0) false
   true)
-                   
+
 (defn p2pkh-script [h160]
   [0x76 0xa9 h160 0x88 0xac])
 
@@ -122,7 +160,21 @@
   (and (= (count cmds) 3)
        (= cmd0 0xa9) (bytes? cmd1) (= (count cmd1) 20) (= cmd2 0x87)))
 
+(defn p2wpkh-script [h160]
+  [0x00 h160])
+
+(defn p2wpkh-script? [[cmd0 cmd1 :as cmds]]
+  (and (= (count cmds) 2)
+       (= cmd0 0x00) (bytes? cmd1) (= (count cmd1) 20)))
+
+(defn p2wsh-script [h256]
+  [0x00 h256])
+
+(defn p2wsh-script? [[cmd0 cmd1 :as cmds]]
+  (and (= (count cmds) 2)
+       (= cmd0 0x00) (bytes? cmd1) (= (count cmd1) 32)))
+
 (defnc address [script testnet?]
   (p2pkh-script? script) (ecc/h160->p2pkh-address (nth script 2) testnet?)
   (p2sh-script? script) (ecc/h160->p2sh-address (nth script 1) testnet?))
-  
+
--- src/project_alchemy/tx.clj
+++ src/project_alchemy/tx.clj
@@ -18,7 +18,7 @@
 (def SIGHASH_NONE 2)
 (def SIGHASH_SINGLE 3)
 
-(defrecord Tx [command-name version tx-ins tx-outs locktime testnet?])
+(defrecord Tx [command-name version tx-ins tx-outs locktime testnet? segwit?])
 (defrecord TxIn [prev-tx prev-index script-sig sequence])
 (defrecord TxOut [amount script-pubkey])
 
@@ -34,15 +34,15 @@
   :let [url (get-url tx-id testnet?)
         response (slurp url)
         raw (hex->bytes (clojure.string/trim response))
-        tx (parse-tx (io/input-stream raw) testnet?)]
-  ;; :do (println (id tx) tx-id)
-  :do (assert (= (id tx) tx-id)
-              (format "IDs don't match %s %s" (id tx) tx-id))
-  ;; Currently assumes it is NOT a segwit transaction
-  ;; Book has hack to work around segwit txs, but seems better to wait
-  ;; and code it correctly
+        tx (parse-tx (io/input-stream raw) testnet?)
+        computed-id (if (:segwit? tx) (id tx)
+                        (bytes->hex (byte-array (reverse (hash256 raw)))))]
+  :do (def pt tx)
+  :do (def pt-raw raw)
+  :do (assert (= computed-id tx-id)
+              (format "IDs don't match %s %s" computed-id tx-id))
   tx)
-;;(def fetch-tx (memoize/fifo fetch-tx))
+(def fetch-tx (memoize/fifo fetch-tx))
 
 ;; parsing and serializing
 
@@ -79,7 +79,7 @@
   (bytes/concat (le-num->bytes 8 amount)
                 (script/serialize-script script-pubkey)))
 
-(defnc parse-tx [^InputStream stream testnet?]
+(defnc parse-tx-legacy [^InputStream stream testnet?]
   :let [version (le-bytes->num (read-bytes stream 4))
         num-tx-ins (read-varint stream)
         tx-ins (vec (for [i (range num-tx-ins)]
@@ -88,9 +88,43 @@
         tx-outs (vec (for [i (range num-tx-outs)]
                        (parse-tx-out stream)))
         locktime (le-bytes->num (read-bytes stream 4))]
-  (->Tx "tx" version tx-ins tx-outs locktime testnet?))
+  (->Tx "tx" version tx-ins tx-outs locktime testnet? false))
 
-(defn serialize-tx ^bytes [{:keys [version tx-ins tx-outs locktime testnet?]}]
+;; (defn peek-stream [^InputStream stream]
+;;   (.mark stream 10)
+;;   (let [b (read-bytes stream 4)]
+;;     (.reset stream)
+;;     (bytes->hex b)))
+
+(defnc parse-tx-segwit [^InputStream stream testnet?]
+  :let [version (le-bytes->num (read-bytes stream 4)),
+        marker (vec (read-bytes stream 2))]
+  (not= marker [0 1]) (throw (ex-info "Not a segwit transaction" {:marker marker}))
+  :let [num-inputs (read-varint stream)
+        inputs (vec (for [_ (range num-inputs)]
+                      (parse-tx-in stream)))
+        num-outputs (read-varint stream)
+        outputs (vec (for [_ (range num-outputs)]
+                       (parse-tx-out stream)))
+        inputs (vec (for [tx-in inputs
+                          :let [num-items (read-varint stream),
+                                items (vec (for [_ (range num-items)
+                                                 :let [item-len (read-varint stream)]]
+                                             (if (= 0 item-len) 0
+                                                 (read-bytes stream item-len))))]]
+                      (assoc tx-in :witness items)))
+        locktime (le-bytes->num (read-bytes stream 4))]
+  (->Tx "tx" version inputs outputs locktime testnet? true))
+
+(defnc parse-tx [^InputStream stream testnet?]
+  :do (.mark stream 10)
+  :let [_ (read-bytes stream 4),
+        segwit-marker (.read stream),
+        parse-method (if (= segwit-marker 0) parse-tx-segwit parse-tx-legacy)]
+  :do (.reset stream)
+  (parse-method stream testnet?))
+
+(defn serialize-tx-legacy ^bytes [{:keys [version tx-ins tx-outs locktime]}]
   (byte-array
    (concat (le-num->bytes 4 version)
            (encode-varint (count tx-ins))
@@ -99,8 +133,29 @@
            (apply concat (for [tx-out tx-outs] (serialize-tx-out tx-out)))
            (le-num->bytes 4 locktime))))
 
+(defn serialize-tx-segwit ^bytes [{:keys [version tx-ins tx-outs locktime]}]
+  (byte-array
+   (concat (le-num->bytes 4 version)
+           [0 1]
+           (encode-varint (count tx-ins))
+           (apply concat (for [tx-in tx-ins] (serialize-tx-in tx-in)))
+           (encode-varint (count tx-outs))
+           (apply concat (for [tx-out tx-outs] (serialize-tx-out tx-out)))
+           (apply concat
+                  (for [{:keys [witness] :as tx-in} tx-ins]
+                    (concat (le-num->bytes 1 (count witness))
+                            (apply concat
+                                   (for [item witness]
+                                     (if (integer? item)
+                                       (le-num->bytes 1 item)
+                                       (concat (encode-varint (count item)) item)))))))
+           (le-num->bytes 4 locktime))))
+
+(defnc serialize-tx [tx]
+  (:segwit? tx) (serialize-tx-segwit tx) (serialize-tx-legacy tx))
+
 (defn hash-tx [tx]
-  (byte-array (reverse (hash256 (serialize-tx tx)))))
+  (byte-array (reverse (hash256 (serialize-tx-legacy tx)))))
 
 (defn id [tx]
   (bytes->hex (hash-tx tx)))
@@ -135,17 +190,81 @@
                   (le-num->bytes 4 SIGHASH_ALL)))]
    (bytes->num (hash256 bytes))))
 
+(defnc hash-prevouts [{:keys [tx-ins]}]
+  :let [all-prevouts (byte-array
+                      (apply concat (for [{:keys [prev-tx prev-index]} tx-ins]
+                                      (concat (reverse prev-tx)
+                                              (le-num->bytes 4 prev-index)))))]
+  (hash256 all-prevouts))
+(def hash-prevouts (memoize/fifo hash-prevouts))
+
+(defnc hash-sequence [{:keys [tx-ins]}]
+  :let [all-sequence (byte-array
+                      (apply concat (for [{:keys [sequence]} tx-ins]
+                                      (le-num->bytes 4 sequence))))]
+  (hash256 all-sequence))
+(def hash-sequence (memoize/fifo hash-sequence))
+
+(defnc hash-outputs [{:keys [tx-outs]}]
+  :let [all-outputs (byte-array
+                     (apply concat (for [tx-out tx-outs]
+                                     (serialize-tx-out tx-out))))]
+  (hash256 all-outputs))
+(def hash-outputs (memoize/fifo hash-outputs))
+
+(defnc sig-hash-bip143 [{:keys [tx-ins version locktime testnet?] :as tx}
+                        input-index redeem-script witness-script]
+  :let [{:keys [prev-tx prev-index sequence] :as tx-in} (nth tx-ins input-index),
+        bytes
+        (byte-array
+         (concat (le-num->bytes 4 version)
+                 (hash-prevouts tx) (hash-sequence tx)
+                 (reverse prev-tx) (le-num->bytes 4 prev-index)
+                 (cond
+                   witness-script (script/serialize-script witness-script)
+                   redeem-script (script/serialize-script
+                                  (script/p2pkh-script (nth redeem-script 1)))
+                   :else (script/serialize-script
+                          (script/p2pkh-script
+                           (nth (script-pubkey-tx-in tx-in testnet?) 1))))
+                 (le-num->bytes 8 (amount-tx-in tx-in testnet?))
+                 (le-num->bytes 4 sequence)
+                 (hash-outputs tx)
+                 (le-num->bytes 4 locktime)
+                 (le-num->bytes 4 SIGHASH_ALL)))]
+  (bytes->num (hash256 bytes)))
+
+
 (defnc verify-tx-in [{:keys [tx-ins testnet?] :as tx} input-index]
   :let [{:keys [script-sig] :as tx-in} (nth tx-ins input-index),
         script-pubkey (script-pubkey-tx-in tx-in testnet?)
-        redeem-script (when (script/p2sh-script? script-pubkey)
-                        (let [cmd (last script-sig),
-                              raw-redeem (bytes/concat (encode-varint (count cmd))
-                                                       cmd)]
-                          (script/parse-script (io/input-stream raw-redeem))))
-        z (sig-hash tx input-index redeem-script),
-        script (concat (:script-sig tx-in) script-pubkey)]
-  (script/evaluate-script script z))
+        [z witness]
+        (cond
+          (script/p2sh-script? script-pubkey)
+          (cond
+            :let [cmd (last script-sig),
+                  raw-redeem (bytes/concat (encode-varint (count cmd))
+                                           cmd)
+                  redeem-script (script/parse-script (io/input-stream raw-redeem))]
+            (script/p2wpkh-script? redeem-script)
+            [(sig-hash-bip143 tx input-index redeem-script nil) (:witness tx-in)]
+            (script/p2wsh-script? redeem-script)
+            (let [cmd (last (:witness tx-in)),
+                  raw-witness (bytes/concat (encode-varint (count cmd)) cmd),
+                  witness-script (script/parse-script (io/input-stream raw-witness))]
+              [(sig-hash-bip143 tx input-index nil witness-script) (:witness tx-in)])
+            :else
+            [(sig-hash tx input-index redeem-script) nil])
+          (script/p2wpkh-script? script-pubkey)
+          [(sig-hash-bip143 tx input-index nil nil) (:witness tx-in)]
+          (script/p2wsh-script? script-pubkey)
+          (let [cmd (last (:witness tx-in)),
+                raw-witness (bytes/concat (encode-varint (count cmd)) cmd),
+                witness-script (script/parse-script (io/input-stream raw-witness))]
+            [(sig-hash-bip143 tx input-index nil witness-script) (:witness tx-in)])
+          :else [(sig-hash tx input-index) nil]),
+        script (concat script-sig script-pubkey)]
+  (script/evaluate-script script z witness))
 
 (defnc verify-tx [{:keys [tx-ins] :as tx}]
   (neg? (fee tx)) false
